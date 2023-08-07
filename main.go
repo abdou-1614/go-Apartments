a4aeaaf70ee14a209a7186ea63d3db7db9c757a0
Abdellah Hattabi <kirakira1614@gmail.com>
2023-08-07 22:56:43 +0100

    first commit

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"go-appointement/routes"
 	"go-appointement/storage"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
 	"github.com/kataras/iris/v12"
 )
@@ -12,6 +13,8 @@ func main() {
 	godotenv.Load()
 	storage.InitializeDb()
 	app := iris.Default()
+
+	app.Validator = validator.New()
 
 	location := app.Party("api/location")
 
