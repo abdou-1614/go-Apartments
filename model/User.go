0742f0189fe71bafdc9578dd8e472d2ae156450f
Abdellah Hattabi <kirakira1614@gmail.com>
2023-08-13 23:01:52 +0100

    first commit

--- model/User.go
+++ model/User.go
@@ -4,10 +4,20 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	FirstName      string `json:"firstName"`
-	LastName       string `json:"lastName"`
-	Email          string `json:"email"`
-	Password       string `json:"password"`
-	SocialLogin    bool   `json:"socialLogin"`
-	SocialProvider string `json:"socialProvider"`
+	FirstName      string   `json:"firstName"`
+	LastName       string   `json:"lastName"`
+	Email          string   `json:"email"`
+	Password       string   `json:"password"`
+	SocialLogin    bool     `json:"socialLogin"`
+	SocialProvider string   `json:"socialProvider"`
+	Role           UserRole `json:"role"`
 }
+
+type UserRole string
+
+const (
+	RoleAdmin     UserRole = "admin"
+	RoleUser      UserRole = "user"
+	RoleLandlords UserRole = "landlords"
+	RoleGuest     UserRole = "guest"
+)
