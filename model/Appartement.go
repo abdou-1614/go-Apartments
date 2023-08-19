be7280c219218f6c9c0a4cfdf8f78d3bed048fc8
Abdellah Hattabi <kirakira1614@gmail.com>
2023-08-19 22:40:49 +0100

    first commit

---
+++ model/Appartement.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/datatypes"
+	"gorm.io/gorm"
+)
+
+type Appartements struct {
+	gorm.Model
+	PropertyID  string         `json:"propertyID"`
+	Unit        string         `json:"unit"`
+	Badrooms    int            `json:"badrooms"`
+	Bathrooms   float32        `json:"bathrooms"`
+	SqFt        int            `json:"sqFt"`
+	Rent        float32        `json:"rent"`
+	Deposit     float32        `json:"deposit"`
+	LeaseLength string         `json:"leaseLength"`
+	AvailableOn time.Time      `json:"availableOn"`
+	Active      *bool          `json:"active"`
+	Description string         `json:"description"`
+	Images      datatypes.JSON `json:"images"`
+	Amenities   datatypes.JSON `json:"amenities"`
+}
