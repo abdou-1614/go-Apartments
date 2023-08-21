d5d96fa49e1b1cd9bda004b649ccb3e23ada005d
Abdellah Hattabi <kirakira1614@gmail.com>
2023-08-21 14:48:42 +0100

    first commit

---
+++ model/Property.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"gorm.io/gorm"
+)
+
+type Property struct {
+	gorm.Model
+	UnitType     string         `json:"unitType"`
+	PropertyType string         `json:"propertyType"`
+	Street       string         `json:"street"`
+	City         string         `json:"city"`
+	State        string         `json:"state"`
+	Zip          int            `json:"zip"`
+	Lat          float32        `json:"lat"`
+	Lng          float32        `json:"lng"`
+	BedroomLow   int            `json:"bedroomLow"`  // calculate based off apartments
+	BedroomHigh  int            `json:"bedroomHigh"` // calculate based off apartments
+	BathroomLow  float32        `json:"bathroomLow"` // calculate based off apartments
+	BathroomHigh float32        `json:"bathroomHigh"`
+	Apartments   []Appartements `json:"apartments"`
+}
