695503ded6aeb2415487771f4d1a551d71e9a149
Abdellah Hattabi <kirakira1614@gmail.com>
2023-08-30 13:08:56 +0100

    first commit

--- model/Property.go
+++ model/Property.go
@@ -6,17 +6,18 @@ import (
 
 type Property struct {
 	gorm.Model
-	UnitType     string         `json:"unitType"`
-	PropertyType string         `json:"propertyType"`
-	Street       string         `json:"street"`
-	City         string         `json:"city"`
-	State        string         `json:"state"`
-	Zip          int            `json:"zip"`
-	Lat          float32        `json:"lat"`
-	Lng          float32        `json:"lng"`
-	BedroomLow   int            `json:"bedroomLow"`  // calculate based off apartments
-	BedroomHigh  int            `json:"bedroomHigh"` // calculate based off apartments
-	BathroomLow  float32        `json:"bathroomLow"` // calculate based off apartments
-	BathroomHigh float32        `json:"bathroomHigh"`
-	Apartments   []Appartements `json:"apartments"`
+	UnitType     string       `json:"unitType"`
+	PropertyType string       `json:"propertyType"`
+	Street       string       `json:"street"`
+	City         string       `json:"city"`
+	State        string       `json:"state"`
+	Zip          int          `json:"zip"`
+	Lat          float32      `json:"lat"`
+	Lng          float32      `json:"lng"`
+	UserID       uint         `json:"userID"`
+	BedroomLow   int          `json:"bedroomLow"`  // calculate based off apartments
+	BedroomHigh  int          `json:"bedroomHigh"` // calculate based off apartments
+	BathroomLow  float32      `json:"bathroomLow"` // calculate based off apartments
+	BathroomHigh float32      `json:"bathroomHigh"`
+	Apartments   []Apartments `json:"apartments"`
 }
