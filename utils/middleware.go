335ce5edd760d078d00d8d15a2cfa2878a1cb106
Abdellah Hattabi <kirakira1614@gmail.com>
2023-08-30 15:02:51 +0100

    first commit

--- utils/middleware.go
+++ utils/middleware.go
@@ -1,25 +1,15 @@
 package utils
 
 import (
-	"strconv"
-
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/middleware/jwt"
 )
 
 func RoleMiddleware(roles ...string) iris.Handler {
 	return func(ctx iris.Context) {
-		params := ctx.Params()
-		id := params.Get("id")
+		//params := ctx.Params()
+		/*id := params.Get("id")*/
 		claims := jwt.Get(ctx).(*AccessToken)
-
-		if strconv.FormatUint(uint64(claims.ID), 10) != id {
-			ctx.JSON(iris.Map{
-				"Message": "Invalid ID",
-				"STATUS":  iris.StatusUnauthorized,
-			})
-			return
-		}
 
 		for _, role := range roles {
 			if string(claims.ROLE) == role {
@@ -27,6 +17,9 @@ func RoleMiddleware(roles ...string) iris.Handler {
 				return
 			}
 		}
-		ctx.StatusCode(iris.StatusForbidden)
+		ctx.JSON(iris.Map{
+			"MESSAGE": "ONLY Landlords and Admin can create Property",
+			"STATUS":  iris.StatusForbidden,
+		})
 	}
 }
