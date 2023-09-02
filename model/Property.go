31d26c5ad8d4655e49b2034f8746cf10b72a51db
Abdellah Hattabi <kirakira1614@gmail.com>
2023-09-02 22:35:03 +0100

    first commit

--- model/Property.go
+++ model/Property.go
@@ -1,23 +1,43 @@
 package model
 
 import (
+	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
 
 type Property struct {
 	gorm.Model
-	UnitType     string       `json:"unitType"`
-	PropertyType string       `json:"propertyType"`
-	Street       string       `json:"street"`
-	City         string       `json:"city"`
-	State        string       `json:"state"`
-	Zip          int          `json:"zip"`
-	Lat          float32      `json:"lat"`
-	Lng          float32      `json:"lng"`
-	UserID       uint         `json:"userID"`
-	BedroomLow   int          `json:"bedroomLow"`  // calculate based off apartments
-	BedroomHigh  int          `json:"bedroomHigh"` // calculate based off apartments
-	BathroomLow  float32      `json:"bathroomLow"` // calculate based off apartments
-	BathroomHigh float32      `json:"bathroomHigh"`
-	Apartments   []Apartments `json:"apartments"`
+	UnitType          string         `json:"unitType"`
+	PropertyType      string         `json:"propertyType"`
+	Street            string         `json:"street"`
+	City              string         `json:"city"`
+	State             string         `json:"state"`
+	Zip               int            `json:"zip"`
+	Lat               float32        `json:"lat"`
+	Lng               float32        `json:"lng"`
+	UserID            uint           `json:"userID"`
+	BedroomLow        int            `json:"bedroomLow"`  // calculate based off apartments
+	BedroomHigh       int            `json:"bedroomHigh"` // calculate based off apartments
+	BathroomLow       float32        `json:"bathroomLow"` // calculate based off apartments
+	BathroomHigh      float32        `json:"bathroomHigh"`
+	RentLow           float32        `json:"rentLow"` // calculate based off apartments
+	RentHigh          float32        `json:"rentHigh"`
+	Name              string         `json:"name"`
+	Amenities         datatypes.JSON `json:"amenities"`
+	IncludedUtilities datatypes.JSON `json:"includedUtilities"`
+	Images            datatypes.JSON `json:"images"`
+	Description       string         `json:"description"`
+	Email             string         `json:"email"`
+	FirstName         string         `json:"firstName"`
+	LastName          string         `json:"lastName"`
+	LaundryType       string         `json:"laundryType"`
+	OnMarket          *bool          `json:"onMarket"`
+	ParkingFee        float32        `json:"parkingFee"`
+	PetsAllowed       string         `json:"petsAllowed"`
+	CountryCode       string         `json:"countryCode"`
+	CallingCode       string         `json:"callingCode"`
+	PhoneNumber       string         `json:"phoneNumber"`
+	Website           string         `json:"website"`
+	Stars             float32        `json:"stars"`
+	Apartments        []Apartments   `json:"apartments"`
 }
