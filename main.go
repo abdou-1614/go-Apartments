2cab3cc84177325288a70c6b4ed459a4f17ea7cd
Abdellah Hattabi <kirakira1614@gmail.com>
2023-09-03 22:15:03 +0100

    first commit

--- main.go
+++ main.go
@@ -72,6 +72,7 @@ func main() {
 		property.Post("/create", accessTokenVerifierMiddleware, utils.RoleMiddleware(string(model.RoleLandlords), string(model.RoleAdmin)), routes.CreateProperty)
 		property.Get("/{id}", routes.GetProperty)
 		property.Delete("/delete/{id}", accessTokenVerifierMiddleware, routes.DeleteProperty)
+		property.Patch("/update/{id}", utils.RoleMiddleware(string(model.RoleLandlords), string(model.RoleAdmin)), routes.UpdateProperty)
 	}
 
 	app.Post("/api/refresh", refreshTokenVerifierMiddleware, utils.RefreshToken)
