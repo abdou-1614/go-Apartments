a31b36b513cf38ded3a17c7b26d83de4cc7d7200
Abdellah Hattabi <kirakira1614@gmail.com>
2023-09-08 22:59:52 +0100

    first commit

--- routes/role_management.go
+++ routes/role_management.go
@@ -34,9 +34,146 @@ func SubmitRoleChangeRequest(ctx iris.Context) {
 		return
 	}
 
-	ctx.JSON(iris.StatusCreated)
+	response := map[string]interface{}{
+		"MESSAGE": "THE REQUEST HAS BEEN SUBMITED SUCCESSFULLY",
+		"STATUS":  iris.StatusCreated,
+	}
+
+	ctx.JSON(response)
+}
+
+func ManageRoleChangeRequests(ctx iris.Context) {
+	claims := jwt.Get(ctx).(*utils.AccessToken)
+
+	if claims.ROLE != model.RoleAdmin {
+		utils.CreateError(iris.StatusForbidden, "NOT ADMIN", "Only admins can manage role change requests", ctx)
+		return
+	}
+
+	var requests []model.RoleChangeRequest
+
+	if err := storage.DB.Where("status = ?", model.RequestPending).Preload("User").Find(&requests).Error; err != nil {
+		utils.CreateInternalServerError(ctx)
+		return
+	}
+
+	var response []RoleChangeRequestWithUser
+
+	for _, request := range requests {
+		response = append(response, RoleChangeRequestWithUser{
+			ID:       request.ID,
+			UserID:   request.UserID,
+			UserName: request.User.FirstName + " " + request.User.LastName,
+			NewRole:  request.User.Role,
+			Status:   request.Status,
+		})
+	}
+
+	ctx.JSON(response)
+}
+
+func AcceptRoleChangeRequest(ctx iris.Context) {
+	claims := jwt.Get(ctx).(*utils.AccessToken)
+
+	if claims.ROLE != model.RoleAdmin {
+		utils.CreateError(iris.StatusForbidden, "NOT ADMIN", "Only admins can manage role change requests", ctx)
+		return
+	}
+
+	requestID, err := ctx.Params().GetUint("id")
+
+	if err != nil {
+		utils.CreateError(iris.StatusBadRequest, "INVALID ID", "INVLAID ID", ctx)
+		return
+	}
+
+	var request model.RoleChangeRequest
+
+	if err := storage.DB.First(&request, requestID).Error; err != nil {
+		utils.CreateError(iris.StatusInternalServerError, "Faild to find", "Faild to Find request", ctx)
+		return
+	}
+
+	request.Status = model.RequestAccepted
+
+	if err := storage.DB.Save(&request).Error; err != nil {
+		utils.CreateError(iris.StatusInternalServerError, "Faild To Save", "Faild to save in DB", ctx)
+		return
+	}
+
+	var user model.User
+
+	if err := storage.DB.First(&user, request.UserID); err != nil {
+		utils.CreateError(iris.StatusInternalServerError, "Faild To Find", "Faild to fetch user", ctx)
+		return
+	}
+
+	user.Role = model.RoleAdmin
+
+	if err := storage.DB.Save(&user).Error; err != nil {
+		utils.CreateError(iris.StatusInternalServerError, "Faild To save new role of user", "Faild To save new role of user in DB", ctx)
+		return
+	}
+
+	response := map[string]interface{}{
+		"MESSAGE": "THE REQUEST HAS BEEN ACCEPTED SUCCESSFULLY",
+		"STATUS":  iris.StatusOK,
+	}
+
+	ctx.JSON(response)
+}
+
+func RejectRoleChangeRequest(ctx iris.Context) {
+	claims := jwt.Get(ctx).(*utils.AccessToken)
+
+	if claims.ROLE != model.RoleAdmin {
+		utils.CreateError(iris.StatusForbidden, "NOT ADMIN", "Only admins can manage role change requests", ctx)
+		return
+	}
+
+	requestID, err := ctx.Params().GetUint("id")
+
+	if err != nil {
+		utils.CreateError(iris.StatusBadRequest, "INVALID ID", "INVLAID ID", ctx)
+		return
+	}
+
+	var request model.RoleChangeRequest
+
+	if err := storage.DB.First(&request, requestID).Error; err != nil {
+		utils.CreateError(iris.StatusInternalServerError, "Faild to find", "Faild to Find request", ctx)
+		return
+	}
+
+	request.Status = model.RequestRejected
+
+	if err := storage.DB.Save(&request).Error; err != nil {
+		utils.CreateError(iris.StatusInternalServerError, "Faild To Save", "Faild to save in DB", ctx)
+		return
+	}
+
+	response := map[string]interface{}{
+		"MESSAGE": "THE REQUEST HAS BEEN REJECTED SUCCESSFULLY",
+		"STATUS":  iris.StatusOK,
+	}
+
+	ctx.JSON(response)
 }
 
 type RoleChangeRequest struct {
 	UserID uint `json:"userID" validate:"required"`
+}
+
+type ManageRoleChange struct {
+	UserID  uint                `json:"userId" validate:"required"`
+	NewRole model.UserRole      `json:"newRole" validate:"required,oneof=user landlords"`
+	Status  model.RequestStatus `json:"status"`
+}
+
+type RoleChangeRequestWithUser struct {
+	ID       uint
+	UserID   uint
+	UserName string
+	NewRole  model.UserRole
+	Status   model.RequestStatus
 }
